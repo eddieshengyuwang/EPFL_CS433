a91916053b4dc82278007b8f3e2669af5423931b
Eddie Wang <eddieshengyuwang@gmail.com>
2018-10-14 00:33:55 +0200

    finished first few fns

---
+++ Project1/implementation.py
@@ -0,0 +1,50 @@
+import numpy as np
+from proj1_helpers import load_csv_data, predict_labels, create_csv_submission
+from created_helpers import batch_iter, least_squares_cost
+
+
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+    '''
+    :param y: labels -> m x 1 np.array
+    :param tx: X -> m x n-1 np.array
+    :param initial_w: weights -> n x 1 np.array
+    :param max_iters: int
+    :param gamma: step size -> float
+    :return: (w, loss) -> (np.array, int)
+    '''
+
+    m = y.shape[0]
+    for i in max_iters:
+        error = np.dot(tx, initial_w) - y  # (m,1)
+        gradient = np.dot(tx.T, error)  # (n,m)x(m,1) -> (n,1)
+        initial_w = initial_w - (gamma / m) * gradient
+
+    cost = least_squares_cost(y, tx, initial_w)
+    return (initial_w, cost)
+
+
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+
+    m = y.shape[0]
+    for i in max_iters:
+        for minibatch_y, minibatch_tx in batch_iter(y, tx, 1): # O(1) runtime
+            error = np.dot(minibatch_tx, initial_w) - minibatch_y  # (1,1)
+            gradient = np.dot(minibatch_tx.T, error)  # (n,1)x(1,1) -> (n,1)
+            initial_w = initial_w - (gamma / m) * gradient
+
+    cost = least_squares_cost(y, tx, initial_w)
+    return (initial_w, cost)
+
+def least_squares(y, tx):
+
+
+    pass
+
+def ridge_regression(y, tx, lambda_):
+    pass
+
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
+    pass
+
+def reg_logistic_regression(y, tx, lambda_, inital_w, max_iters, gamma):
+    pass
