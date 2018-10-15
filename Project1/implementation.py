8891bd79af8dc5e3fdf79afab4f4581aef056d40
Eddie Wang <eddieshengyuwang@gmail.com>
2018-10-15 12:53:23 +0200

    finish all but log regr

--- Project1/implementation.py
+++ Project1/implementation.py
@@ -1,7 +1,10 @@
-import numpy as np
-from proj1_helpers import load_csv_data, predict_labels, create_csv_submission
-from created_helpers import batch_iter, least_squares_cost
+'''
+NOTE that in our implementation, we use m to represent # rows
+and n to represent # columns
+'''
 
+import numpy as np
+from created_helpers import batch_iter, least_squares_cost
 
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     '''
@@ -33,13 +36,32 @@ def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
             initial_w = initial_w - (gamma / m) * gradient
 
     cost = least_squares_cost(y, tx, initial_w)
+
     return (initial_w, cost)
 
 def least_squares(y, tx):
-    pass
+    # equation is (X.T*X)^(-1) * (X.T*y)
+
+    # question for TA's, should we use np.linalg.solve instead of invert?
+
+    first_term = np.linalg.inv(np.dot(tx.T,tx)) # inv((n,m)x(m,n)) -> (n,n)
+    second_term = np.dot(tx.T,y) # (n,m)x(m,1) -> (n,1)
+    return np.dot(first_term, second_term) # (n,n)x(n,1) -> (n,1)
 
 def ridge_regression(y, tx, lambda_):
-    pass
+    # equation is (X.T*X + (lambda')*I)^(-1) * (X.T*y)
+    # where lambda' = 2*m*lambda_
+
+    # question for TA's, should we use np.linalg.solve instead of invert?
+
+    m = tx.shape[0]
+    lambda_prime = 2*m*lambda_ # int
+
+    xT_x = np.dot(tx.T, tx) # (n,m)x(m,n) -> (n,n)
+    lambda_I = np.dot(lambda_prime, np.eye(n)) # int * (n,n) -> (n,n)
+    xT_y = np.dot(tx.T, y) # (n,m)x(m,1) -> (n,1)
+
+    return np.dot(np.linalg.inv(xT_x + lambda_I),xT_y) # (n,n)x(n,1) -> (n,1)
 
 def logistic_regression(y, tx, initial_w, max_iters, gamma):
     pass
